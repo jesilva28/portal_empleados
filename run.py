55260f1146e3670be38fb686bb604706da234708
unknown <jesilva@uninorte.edu.co>
2022-10-12 22:37:19 -0500

    docker deployment

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 from app import app, db 
 if __name__=="__main__":
-<<<<<<< HEAD
-    app.run()
-=======
-    app.run()
->>>>>>> 9e6ad170f6393e3ba8bc4e084b00451fc8f38193
+    app.run(debug=True, host="0.0.0.0", port=5000)
